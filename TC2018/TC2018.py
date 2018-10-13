45a09c6cbadb4e76912128ee9fe5c78bd3833626
Thibault Hilaire <thibault.hilaire@lip6.fr>
2018-10-13 17:36:41 +0200

    add the four examples

--- TC2018/TC2018.py
+++ TC2018/TC2018.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from fixif.LTI import Filter
+from fixif.LTI import Filter, random_Elliptic
 from fixif.Structures import State_Space
 from fixif.FXPF import FXPF_ABCD
 
@@ -48,9 +48,24 @@ A, B, C, D = [mat(d[x]) for x in ('a', 'b', 'c', 'd')]
 S2 = State_Space(Filter(A=A, B=B.transpose(), C=C, D=D, name='longitudinal oscillation controller DL'))
 
 
+# SDR example (from Fig. 15 "Software-Defined Radio FPGA Cores: Building towards a Domain-Specific Language")
+# Fs=10kHz, Fstop1=2.190, Fpass1=2.1972, Fpass2=2.1974, Fstop2=2.210, Astop1=200dB, Apsass=0.1dB, Astop2=200dB
+# This filter comes from a testbench in Software-Defined Radio system~\cite[Fig. 15]{}. It a 6th order Butterworth filter designed with the following parameters: sampling frequency = 10 kHz, lower cutoff frequency = 2.190 kHz, higher cutoff frequency = 2.210 kHz, passband ripple = 0.1 dB, stopband attenuation = 200 dB.
+d = loadmat('SDR.mat')
+#WARNING: this filter is designed with SOS-structure. When converted to state-space (by matlab), I am not sure if the spectral radius is lower than 1
+#(when fdatool makes the single-structure conversion, the filter is not stable anymore)
+A, B, C, D = [mat(d[x]) for x in ('A', 'B', 'C', 'D')]
+S3 = State_Space(Filter(A=A, B=B.transpose(), C=C, D=D, name='Software-Defined Radio'))
 
 
+# random Elliptic filter
+F4 = random_Elliptic(seed=12345, quant=16)
+print(F4.details())
+S4 = State_Space(F4)
 
+
+for S in (S1, S2, S3, S4):
+	testFxPF(S)
 
 
 
